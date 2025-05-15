5a8f9448356b444a3ce4ac2d6fbe22d4f74a0546
wade0426 <laix678@gmail.com>
2025-05-16 00:16:58 +0800

    新增_取得停水公告方法
    修正_過濾停水公告函數

--- water_gpt/WaterOutageQuery/Tools.py
+++ water_gpt/WaterOutageQuery/Tools.py
@@ -3,7 +3,22 @@ import json
 import requests
 import os
 
+
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0"
+headers = {
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'accept-language': 'zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'content-type': 'application/json',
+    'origin': 'https://web.water.gov.tw',
+    'priority': 'u=1, i',
+    'referer': 'https://web.water.gov.tw/wateroff/city/%E8%87%BA%E4%B8%AD%E5%B8%82/index.html',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0',
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+# 注意替換
+FolderPath = "D:/Python/NLP_LAB/water/water_gpt/water_gpt/WaterOutageQuery/County_data"
+
 
 def get_date_range(days=30):
     """
@@ -36,11 +51,12 @@ def get_date_range(days=30):
 
 def remove_coordinates_from_water_off_area(data_list: list):
     """
-    移除列表中每個項目中 'waterOffArea' 字典內的 'coordinates' 鍵。
+    移除列表中每個項目中 'waterOffArea' 和 'pressureDownArea' 字典內的 'coordinates' 鍵。
 
     Args:
         data_list (list): 包含一個或多個字典的列表，
-                          每個字典可能包含 'waterOffArea' 鍵。
+                          每個字典可能包含 'waterOffArea' 鍵
+                          或 'pressureDownArea' 鍵。
     Returns:
         list: 修改後的列表。
     """
@@ -50,6 +66,10 @@ def remove_coordinates_from_water_off_area(data_list: list):
             # 如果 'coordinates' 存在於 'waterOffArea' 中，則移除它
             # 使用 pop 並提供一個預設值 (None) 可以避免在鍵不存在時出錯
             item['waterOffArea'].pop('coordinates', None)
+        if 'pressureDownArea' in item and isinstance(item['pressureDownArea'], dict):
+            # 如果 'coordinates' 存在於 'pressureDownArea' 中，則移除它
+            # 使用 pop 並提供一個預設值 (None) 可以避免在鍵不存在時出錯
+            item['pressureDownArea'].pop('coordinates', None)
     return data_list
 
 
@@ -80,7 +100,7 @@ def extract_values_simple(data_list: list) -> list:
   return [item["value"] for item in data_list if isinstance(item, dict) and "value" in item]
 
 
-def get_town_data(FolderPath: str):
+def get_town_data():
     """
     獲取所有縣市行政區資料
     """
@@ -109,12 +129,34 @@ def get_town_data(FolderPath: str):
             f.write(json.dumps(data, ensure_ascii=False, indent=4))
 
 
+def get_water_outage_notices():
+    """
+    獲取停水公告
+    """
+    # 取得當前時間, 30天後的時間
+    start_date, end_date = get_date_range()
+    json_data = {
+        'mode': 1,
+        'startDate': start_date,
+        'endDate': end_date,
+    }
+    response = requests.post('https://web.water.gov.tw/wateroffapi/f/case/search', headers=headers, json=json_data)
+    # 移除 'waterOffArea' 字典內的 'coordinates' 鍵
+    response = remove_coordinates_from_water_off_area(response.json())
+
+    # 要退一層
+    with open(os.path.join(FolderPath, f"../water_outage_notices.json"), "w", encoding="utf-8") as f:
+        # 將 list 轉換為 json 字串 並保留換行和縮排
+        f.write(json.dumps(response, ensure_ascii=False, indent=4))
+
+
 if __name__ == "__main__":
     # get_date_range()
     # remove_items_with_values(data)
     # remove_coordinates_from_water_off_area(data)
 
-    # FolderPath = "D:/Python/NLP_LAB/water/water_gpt/water_gpt/WaterOutageQuery/County_data"
-    # get_town_data(FolderPath=FolderPath)
+    # get_town_data()
+
+    get_water_outage_notices()
 
     pass
