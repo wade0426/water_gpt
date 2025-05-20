76da85547b4b16d630006aa51de72699d8cc2771
wade0426 <laix678@gmail.com>
2025-05-20 16:20:38 +0800

    停水資料更新

--- water_gpt/WaterOutageQuery/api-test.py
+++ water_gpt/WaterOutageQuery/api-test.py
@@ -3,9 +3,9 @@ URL = "http://localhost:8002/water-outage-query"
 import requests
 import json
 
-response = requests.get(URL, params={"affectedCounties": "66000", "affectedTowns": "66000080"})
-# response = requests.get(URL, params={"affectedCounties": "66000"})
+# response = requests.get(URL, params={"affectedCounties": "66000", "affectedTowns": "66000080"})
+response = requests.get(URL, params={"affectedCounties": "66000"})
 print(response.json())
 
-# with open("response_test.json", "w", encoding="utf-8") as f:
-#     json.dump(response.json(), f, ensure_ascii=False, indent=4)
+with open("response_test.json", "w", encoding="utf-8") as f:
+    json.dump(response.json(), f, ensure_ascii=False, indent=4)
